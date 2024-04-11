c589bfa28f3b9b546ff804922a20bf6d6fddb89f
hwang <hjeonghan0@gmail.com>
2024-04-11 18:02:05 +0900

    first commit

---
+++ chap01-literal-and-variable/src/main/java/com/ohgiraffers/section01/literal/Application01.java
@@ -0,0 +1,22 @@
+package com.ohgiraffers.section01.literal;
+
+public class Application01 {
+
+    public static void main(String[] args) {
+        //리터럴이란 값 자체를 의미한다.
+
+        // 정수형 리터럴
+        System.out.println(123);
+        //실수형 리터럴
+        System.out.println(12.34);
+        //문자형 리터럴
+        System.out.println('A');
+        //문자열형 리터럴
+        System.out.println("hello wlord");
+        //"12345" 문자열
+
+        //논리형 리터널
+        System.out.println(true);
+        System.out.println(false);
+    }
+}
---
+++ chap01-literal-and-variable/src/main/java/com/ohgiraffers/section01/literal/Application02.java
@@ -0,0 +1,36 @@
+package com.ohgiraffers.section01.literal;
+
+public class Application02 {
+    public static void main(String[] args) {
+        // 2+2
+        System.out.println(2+2);
+        // 2-2
+        System.out.println(2-2);
+        // 2/2
+        System.out.println(2/2);
+        // 2*2
+        System.out.println(2*2);
+        // 123 % 10 몫연산
+        System.out.println(123%1.0);
+
+
+        //1.23 + 1.23
+        System.out.println(1.23+1.23);
+        //1.23 - 1.23
+        System.out.println(1.23-1.23);
+        //1.23 * 1.23
+        System.out.println(1.23*1.23);
+        //1.23 / 1.23
+        System.out.println(1.23/1.23);
+        //1.23 % 1.0
+        System.out.println(1.23%1.0);
+
+        // "123" + "12"
+        System.out.println("123"+"12");
+        // 나머지 뺴기 나누기 곱하기는 안된다 하지만 "123"+12 는 가능
+        System.out.println();
+
+
+
+    }
+}

01c8de5d1a0bbf23f13cd06bfaa85c918aa0d31f
hwang <hjeonghan0@gmail.com>
2024-04-12 17:09:41 +0900

    2024.4.12.

--- chap01-literal-and-variable/src/main/java/com/ohgiraffers/section01/literal/Application02.java
+++ chap01-literal-and-variable/src/main/java/com/ohgiraffers/section01/literal/Application02.java
@@ -1,7 +1,7 @@
 package com.ohgiraffers.section01.literal;
 
 public class Application02 {
-    public static void main(String[] args) {
+    public static void main(String[]args){
         // 2+2
         System.out.println(2+2);
         // 2-2
@@ -28,7 +28,7 @@ public class Application02 {
         // "123" + "12"
         System.out.println("123"+"12");
         // 나머지 뺴기 나누기 곱하기는 안된다 하지만 "123"+12 는 가능
-        System.out.println();
+
 
 
 

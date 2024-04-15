1e7b5e8c5aa904838b5efc94d13df9f92d45bd0f
hwang <hjeonghan0@gmail.com>
2024-04-15 17:55:06 +0900

    2024.04.15

--- chap02-operator/src/main/java/com/ohgiraffers/section06/ternary/Application01.java
+++ chap02-operator/src/main/java/com/ohgiraffers/section06/ternary/Application01.java
@@ -7,8 +7,7 @@ public class Application01 {
         * 자바에서 유일하게 피연산자 항목이 3개인 연산자이다.
         * 항목이 3개임 : (조건식)? 참일 경우 : 거짓일 경우
         * 조건식은 반드시 결과가 true || false 가 나오도록 해야한다.
-        *
-        * */
+        **/
         int num = 10;
         int num2 = -10;
         // num1 일과 num2를 삼항 연산자를 이용하여 양수 또는 음수로 표현해 주세요.
@@ -31,8 +30,9 @@ public class Application01 {
          String result5 = (num5 > 0)? "양수이다" : (num5 == 0)? "0이다" : "음수이다.";
          System.out.println(("num5는 : ") + result5);
 
-
-
+         int num6 = 10; // 숫자바꿀때마다
+         String result6 = (num6 == 0)? "0입니다" : (num6 == 1)? "1입니다" : (num6 == 2)? "2입니다" : (num6 == 3)? "3입니다" : (num6 > 3)? "3초과입니다" : "음수이거나 숫자가아닙니다.";
+         System.out.println(result6);
 
 
     }
